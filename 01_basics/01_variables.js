bbb107a377effdfa030c7efee0add47760bc8d39
alif-786 <kamatealip1@gmail.com>
2025-01-04 11:15:54 +0530

    01_variables.js created

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 155232;
+let accountEmail = 'alip@gmail.com'
+var accountPassword = 'alip1234@'
+accountCity = 'sangli'
+
+
+
+// accountId = 13012; // this will give us an error. not allowed
+// console.log(`account id : ${accountId}`);
+
+accountEmail = 'John@gmail.com'
+accountPassword = 'john123@lovesJane'
+accountCity = 'Pune'
+
+console.table({"Account id" : accountId, "Account email" : accountEmail, "Account password":accountPassword, "Account city": accountCity})
+
