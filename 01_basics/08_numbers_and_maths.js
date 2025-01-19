cb8aff8f2a8b193e6fa4aa73dc7819f370d10fab
kamatealif <kamatealip1@gmail.com>
2025-01-19 11:31:28 +0530

    learn 08_numbers_and_maths

---
+++ 01_basics/08_numbers_and_maths.js
@@ -0,0 +1,19 @@
+let score = 100
+console.log("score: ", score)
+
+let balance = new Number(18540);
+console.log("balance: ", balance)
+
+//toString()
+console.log(balance.toString().length)
+
+//toFixed()
+console.log(balance.toFixed(2))
+
+//toPrecision()
+let anotherNumber = 138.887
+console.log(anotherNumber.toPrecision(3))
+
+//toLocalString()
+let numberOfStudents = 10000000
+console.log(numberOfStudents.toLocaleString('en-IN'))
