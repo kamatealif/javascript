b8df3a72724b7a54c9b4f7c114e0229b372b3ad6
kamatealif <kamatealip1@gmail.com>
2025-01-24 08:42:42 +0530

    learn Math.random()

--- 01_basics/08_numbers_and_maths.js
+++ 01_basics/08_numbers_and_maths.js
@@ -17,3 +17,23 @@ console.log(anotherNumber.toPrecision(3))
 //toLocalString()
 let numberOfStudents = 10000000
 console.log(numberOfStudents.toLocaleString('en-IN'))
+
+// Math library
+console.log(Math)
+console.log(Math.abs(-11)); // will conver -ve to +ve
+console.log(Math.round(4.3)) // will convert to nearest integer
+console.log(Math.ceil(4.2)) // will convert to next integer
+console.log(Math.floor(4.9)) // will convert to previous integer
+
+console.log(Math.min(2,3,2,3402,29,1,20,109,2093))
+
+//======================================== Math.Random() ========================================
+
+console.log(Math.random())
+console.log(Math.random() * 10)
+console.log(Math.round(Math.random() * 10) + 1) // 1 to 10
+
+// formula for the range of random number
+const minVal = 10
+const maxVal = 20
+console.log(Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal )
