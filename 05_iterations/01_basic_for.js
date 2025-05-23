f6e650b454653b7c69e14d811a97ef98c5b0c032
kamatealif <kamatealip1@gmail.com>
2025-05-23 22:02:58 +0530

    nested loops

--- 05_iterations/01_basic_for.js
+++ 05_iterations/01_basic_for.js
@@ -1,5 +1,9 @@
 // for 
-for(let index = 0; index < 10; index++){
+for(let index = 0; index <= 10; index++){
     const element = index;
+    if(element == 7 ){
+        console.log("7 Is the Best Number:💎💎")
+        continue
+    }
     console.log(element)
 }
---
+++ 05_iterations/02_nested_for.js
@@ -0,0 +1,7 @@
+for(let index = 0; index <=10; index++){
+    console.log(`Outer loop values ${index}...`)
+    for (let jIndex = 1; jIndex <= 5; jIndex++){
+        console.log(`inner loop values ${jIndex}`)
+    }
+    console.log("--------------------------------------------")
+}
